4478da1134b443f27d2683079f49270b52d65a33
André <ruland@outlook.de>
2022-03-09 12:07:22 +0100

    Super simples Singelton

---
+++ HalloSingelton/HalloSingelton/Logger.cs
@@ -0,0 +1,30 @@
+﻿namespace HalloSingelton
+{
+    public class Logger
+    {
+        private static Logger? _instance;
+        private static object _instanceLock = new();
+        public static Logger Instance
+        {
+            get
+            {
+                lock (_instanceLock)
+                {
+                    if (_instance == null)
+                        _instance = new Logger();
+                }
+                return _instance;
+            }
+        }
+
+        private Logger()
+        {
+            Console.WriteLine("Neue Logger Instance");
+        }
+
+        public void Info(string msg)
+        {
+            Console.WriteLine($"[INFO] {DateTime.Now:d} {DateTime.Now:t} {msg}");
+        }
+    }
+}
---
+++ HalloSingelton/HalloSingelton/Program.cs
@@ -0,0 +1,14 @@
+﻿using HalloSingelton;
+
+Console.WriteLine("Hello Singelton");
+
+Parallel.For(0, 10, i => Logger.Instance.Info($"Hallo Logger {i}"));
+
+//for (int i = 0; i < 10000; i++)
+//{
+//    Task.Run(() => Logger.Instance.Info($"Hallo Logger {i}"));
+//}
+
+
+Console.WriteLine("Ende");
+Console.ReadLine();
