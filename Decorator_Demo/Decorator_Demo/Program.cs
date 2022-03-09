c1ef56aff1d867d8898c66bebd792e9aead0650a
André <ruland@outlook.de>
2022-03-09 14:40:23 +0100

    Deko in hübsch

--- Decorator_Demo/Decorator_Demo/Program.cs
+++ Decorator_Demo/Decorator_Demo/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Decorator_Demo
 {
@@ -10,24 +6,10 @@ namespace Decorator_Demo
     {
         static void Main(string[] args)
         {
-            var lecker1 = new Trüffel(new Trüffel( new Salami(new Käse(new Pizza()))));
+            var lecker1 = new Schinken(new Trüffel(new Salami(new Käse(new Pizza()))));
 
             Console.WriteLine(lecker1.Beschreibung);
             Console.WriteLine(lecker1.Preis);
-
-
-            IComponent MichisLieblingspizza = new Pizza();
-
-            for (int i = 0; i < 100; i++)
-            {
-                MichisLieblingspizza = new Käse(MichisLieblingspizza);
-                // Pizza mit 100 Käse
-            }
-
-            Console.WriteLine(MichisLieblingspizza.Beschreibung);
-            Console.WriteLine(MichisLieblingspizza.Preis);
-
-            // Aspektorientiertem Programmieren
 
             Console.WriteLine("---ENDE---");
             Console.ReadKey();
