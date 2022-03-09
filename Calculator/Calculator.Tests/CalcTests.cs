59cb2e6ca6b2f0129504cad59203d59e836fe061
André <ruland@outlook.de>
2022-03-09 16:24:32 +0100

    Tests sind cool

---
+++ Calculator/Calculator.Tests/CalcTests.cs
@@ -0,0 +1,59 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Calculator.Tests
+{
+    [TestClass]
+    public class CalcTests
+    {
+        [TestMethod]
+        public void Sum_2_and_3_results_5()
+        {
+            //Arrange
+            var calc = new Calc();
+
+            //Act
+            var result = calc.Sum(2, 3);
+
+            //Assert
+            Assert.AreEqual(5, result);
+        }
+
+        [TestMethod]
+        public void Sum_0_and_0_results_0()
+        {
+            //Arrange
+            var calc = new Calc();
+
+            //Act
+            var result = calc.Sum(0, 0);
+
+            //Assert
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        [DataRow(0, 0, 0)]
+        [DataRow(1, 2, 3)]
+        [DataRow(-1, -2, -3)]
+        public void Sum_with_normal_results(int a, int b, int exp)
+        {
+            //Arrange
+            var calc = new Calc();
+
+            //Act
+            var result = calc.Sum(a, b);
+
+            //Assert
+            Assert.AreEqual(exp, result);
+        }
+
+        [TestMethod]
+        public void Sum_MAX_and_1_throws_OverflowException()
+        {
+            var calc = new Calc();
+
+            Assert.ThrowsException<OverflowException>(() => calc.Sum(int.MaxValue, 1));
+        }
+    }
+}
